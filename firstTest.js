ee48ab0fc3628abbfcd74417ee5822b6feffb49a
Henry <henrybethwaite22@gmail.com>
2023-03-14 08:39:12 +1300

    small fixes on test one, added test two

--- firstTest.js
+++ firstTest.js
@@ -11,19 +11,19 @@ rect(0, 0, width+1, height+1);
 
 /*----method for altering cur_frac made with help 
 ------from my friend Popopz */
-cur_frac = cur_frac*24;
+loop_frac = cur_frac*24;
 
-if(cur_frac > 12){
-  cur_frac = 12 - (cur_frac % 12)
+if(loop_frac > 12){
+  loop_frac = 12 - (loop_frac % 12)
 }
 
 //console.log(cur_frac);
 
 for (let j = 1 ; j < 35 ; j++) {
     for (let i = 1 ; i < 94 ; i++) {
-        let noiseTerrain = getNoiseValue((xpos*i)+(cur_frac*1), (ypos*j)+(cur_frac*1.1), 0.5, "gridNoise", 0, 5, 50);
+        let noiseTerrain = getNoiseValue((xpos*i)+(loop_frac*1), (ypos*j)+(loop_frac*1.1), 0.5, "gridNoise", 0, 5, 50);
         fill(255);
-        text(char[int(noiseTerrain)],xpos+(xstep*i), ypos+(ystep*j), 20, 20);
+        !text(char[int(noiseTerrain)],xpos+(xstep*i), ypos+(ystep*j), 20, 20);
     }
   }
 
